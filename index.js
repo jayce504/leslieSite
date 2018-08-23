f9574602b2ad84e32bfb6b6b539574d657614739
Scott Hudson <shudson5523@gmail.com>
2018-08-23 06:30:03 -0400

    updated index.js to include port assigned by heroku and local port 3000 in .listen function

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ app.set('view engine', 'ejs');
 
 app.use(express.static(__dirname + "/public"));
 
-app.listen(3000, () => console.log('Site launched on port 3000!'))
+app.listen(process.env.PORT || 3000)
 
 module.exports = app;
